b1ae2bb87e5fe8e13bd8a28add3fa958b99c10e2
Lizkwong <elizabethokwong@gmail.com>
2023-09-12 21:52:52 +0100

    local storage

---
+++ index.js
@@ -0,0 +1,67 @@
+// let anBot = /ca/;
+// console.log(anBot.match(...'cat car'));
+//  Reading Note
+//  .test returns a boolean which tells you whether the string contains a match or not 
+
+
+// Question 1
+// let regex = /ca/;
+// console.log(regex.test('car cat'));
+
+// let arr = ['car', 'cat'];
+// let object = /ca/;
+
+// let output = arr.forEach(many => {
+//   console.log(object.test(many))
+// })
+
+
+// Question 2
+// let pWord = /po/;
+// console.log(pWord.test('pop prop'));
+
+// let pArray = ['pop','prop'];
+// let pWord = /po/;
+// let result = pArray.forEach(alpha => {
+//   console.log(pWord.test(alpha))
+// })
+
+// Question 3
+// // let fWord = /fr/;
+// // console.log(fWord.test('ferret ferry ferrari'));
+
+// let fArray = ['ferret','ferry','ferrari'];
+// let fWord = /fe/;
+// let output = fArray.forEach(beta => {
+//   console.log(fWord.test(beta))
+// })
+
+
+// Question 4
+// let vowel = /ious/;
+// console.log(vowel.test('malicious loquacious concious'));
+
+// let vowelArray = ['malicious', 'loquacious', 'concious'];
+// let vowel = /ious/;
+// let output = vowelArray.forEach(beta => {
+//   console.log(vowel.test(beta))
+// })
+
+// Question 5
+// let specialChar = / .,:;/;
+// console.log(specialChar.test('what is the reason do not have your own peace of mind, you should never give up.'));
+
+// let charArray = ['what is the reason do not have your own peace of mind? you should never give up.'];
+// let specialChar = / .,:;/;
+// let output = charArray.forEach(beta => {
+//   console.log(specialChar.test(beta))
+// })
+
+// let arr = ['bat', 'cat', 'pet', 'pEd', 'peek'];
+// let regular = /\b[^eE\s]+\b/;
+
+// let output = arr.forEach(word => {
+//   console.log(regular.test(word));
+// });
+
+
